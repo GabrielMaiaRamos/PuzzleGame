5cb62dc4e0c280919f25f0f64cdd8c656a6ef030
André Lopes <alglopes@id.uff.br>
2026-01-19 00:23:56 -0300

    (update)
    a porta abre e fecha

--- PuzzleGame/Assets/PROJETO/Scripts/ObjetctRotate.cs
+++ PuzzleGame/Assets/PROJETO/Scripts/ObjetctRotate.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class ObjetctRotate : MonoBehaviour
 {
+    private bool open = false;
     public void DoorInteract(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            gameObject.transform.Rotate(0, -10, 0);
+            if (open)
+            {
+                gameObject.transform.Rotate(0, 110, 0);
+                open = false;
+            }
+            else
+            {
+                gameObject.transform.Rotate(0, -110, 0);
+                open = true;
+            }
         }
     }
 }
